fe4df008ca3499098da30799080b9d7eadfe43c0
Artem Myachkov <myachkovaa@sushivesla.team>
2024-07-08 14:41:13 +0300

    Initial Commit

---
+++ SPM_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SPM_Test
+//
+//  Created by tteslatess on 08.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
